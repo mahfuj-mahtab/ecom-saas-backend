4b13f284e228a7c28f53c7cc545d1271dbfabc5b
Mahfuj Mahtab Mohot <mohotmohot1@gmail.com>
2025-01-19 15:16:23 +0600

    working in model

--- apps/org/models.py
+++ apps/org/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Organization(models.Model):
+    name = models.TextField(max_length=250)
+    description = models.TextField(max_length=1000)
+    created_by = models.ForeignKey(User,on_delete=models.CASCADE)
+    sub_domain = models.CharField()
+    is_multivendor = models.BooleanField(default=False)
+    logo = models.ImageField(upload_to='/media/upload/')
+    is_active = models.BooleanField(default=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
--- apps/vendor/models.py
+++ apps/vendor/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from org.models import *
 # Create your models here.
+class vendor(models.Model):
+    status = (
+        ('ACTIVE', 'ACTIVE'),
+        ('INACTIVE', 'INACTIVE'),
+        ('SUSPEND', 'SUSPEND'),
+    )
+    name = models.CharField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    org = models.ForeignKey(Organization,on_delete=models.CASCADE)
+    description = models.TextField(max_length=  1000)
+    logo = models.ImageField(upload_to="/media/upload/")
+    contact_info = models.CharField()
+    status = models.CharField(choices=status, default='INACTIVE')
+    rating = models.FloatField(default=0)
+    
+    def __str__(self):
+        return self.name
